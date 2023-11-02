edba9d82bfba0c54afe5ad8ca73ddf050094fb91
hyewon-han <hw1635@hanmail.net>
2023-11-02 14:23:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const initialState = ["apple", "banana", "cherry", "date", "elderberry"];
   const [array, setArray] = useState(initialState);
-  const [result, setResult] = useState([]);
+  const [result, setResult] = useState();
   const [query, setQuery] = useState([]);
   const handleForEach = () => {
     let temp = "";
@@ -18,7 +17,7 @@ function App() {
     setResult(array.map((item) => item.toUpperCase()).join(", "));
   const handleReduce = () => setResult(array.reduce((arr, cur) => arr + cur));
   const handlePush = () => {
-    if (query == "") return;
+    if (query === "") return;
     const newArr = [...array, query];
     setArray(newArr);
     setResult(newArr.join(", "));
@@ -38,6 +37,27 @@ function App() {
     newArr.splice(2, 2, "kiwi", "lime");
     setResult(newArr.join(", "));
   };
+  const handleIndexOf = () => {
+    setResult(array.indexOf(query));
+  };
+  const handleIncludes = () => {
+    setResult(array.includes(query));
+  };
+  const handleFind = () => {
+    setResult(array.find((item) => item.includes(query)));
+  };
+  const handleSome = () => {
+    setResult(array.some((item) => item.includes(query)));
+  };
+  const handleEvery = () => {
+    setResult(array.every((item) => item.length >= 2));
+  };
+  const handleSort = () => {
+    setResult(array.sort());
+  };
+  const handleJoin = () => {
+    setResult(array.join("💚"));
+  };
   return (
     <div className="App">
       <h1>배열 메서드 활용</h1>
@@ -56,13 +76,20 @@ function App() {
       <button onClick={handlePop}>pop</button>
       <button onClick={handleSlice}>slice</button>
       <button onClick={handleSplice}>splice</button>
+      <button onClick={handleIndexOf}>indexOf</button>
+      <button onClick={handleIncludes}>includes</button>
+      <button onClick={handleFind}>find</button>
+      <button onClick={handleSome}>some</button>
+      <button onClick={handleEvery}>every</button>
+      <button onClick={handleSort}>sort</button>
+      <button onClick={handleJoin}>join</button>
       <div>
         <h3>원본배열</h3>
         <p>{array.join(", ")}</p>
       </div>
       <div>
         <h3>결과물</h3>
-        <p>{result}</p>
+        <p>{`${result}`}</p>
       </div>
     </div>
   );
